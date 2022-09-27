331151383e68333a3d90b504ecd57ba3de2d1520
Marlon Garcia <marlongarciab@hotmail.com>
2022-09-27 12:40:50 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,14 +1,14 @@
 public class Player {
 
-    private String player1;
-    private String player2;
+    private String player1;             //instance variable for player 1
+    private String player2;             //instance variable for player 2   
 
-    public Player(String player1, String player2) {
+    public Player(String player1, String player2) {      //Constructor
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public String getPlayer1() {
+    public String getPlayer1() {        //Getters and setters for player 1 and 2
         return player1;
     }
 
