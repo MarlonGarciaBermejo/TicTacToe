62672fec56b5e5eb281be4f69d43149a8701585e
Marlon Garcia <marlongarciab@hotmail.com>
2022-10-04 19:37:26 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-public class Main{
-public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
+
+        GameBoard gameBoard = new GameBoard();
+       Computer computer = new Computer();
+       Player player = new Player();
 
     }
 }
