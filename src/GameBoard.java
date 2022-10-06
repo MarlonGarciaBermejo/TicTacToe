2363ae4eced9254320f6d89573482593bfacf9d4
Marlon Garcia <marlongarciab@hotmail.com>
2022-10-06 10:15:20 +0200

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class GameBoard {    
  /*    _ | _ | _
        _ | _ | _
-       _ | _ | _
+         |   | 
 
        positions in the grid
        [0][0]=1  [0][1]=2 [0][2]=3
